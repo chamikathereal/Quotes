dd10d2af38ea75f258961cab105c7d9c70867309
Chamika Gayashan <dmforceeg@gmail.com>
2024-09-25 00:30:58 +0530

    Almost Done

--- themes/bootstrap-css/views/layouts/app.blade.php
+++ themes/bootstrap-css/views/layouts/app.blade.php
@@ -16,8 +16,6 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Scripts -->
 </head>
 
 <body>
